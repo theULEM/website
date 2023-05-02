6aea64a188e4f45664c943dc989d61cf6c4b8821
Dele Omotosho <dele@authoritywit.com>
2023-05-02 13:47:23 -0400

    chore: adds the new board members

--- src/constants/boards.js
+++ src/constants/boards.js
@@ -1,232 +1,268 @@
 export const executiveCommittee = [
-  {
-    name: "Joseph D. Feaster Jr.",
-    job: "Chairman of the ULEM Board, Of Counsel",
-    organization: "McKenzie & Associates, P.C.",
-  },
-  // {
-  //   name: "Derek E. Brooks",
-  //   job: "Secretary of the ULEM Board, Entrepreneur & Business Professional",
-  //   organization: "",
-  // },
-  {
-    name: "Peter Dulac",
-    job: "Chair of the ULEM Program Committee, Vice President/General Manager",
-    organization: "Enterprise Holdings-Boston Group",
-  },
-  {
-    name: "Nikhil Goray",
-    job: "Chair of the ULEM Development Committee, Principal (Partner)",
-    organization: "Deloitte Consulting",
-  },
-  {
-    name: "Jeffrey L. Hirsch",
-    job:
-      "Chair of the Personnel and Issue & Advocacy Committees, Attorney",
-    organization: "Hirsch Roberts Weinstein LLP",
-  },
-  {
-    name: "Vincent Loporchio",
-    job:
-      "Chair of the ULEM Nominating Committee, Senior Vice President Corporate Communications",
-    organization: "Fidelity Investments",
-  },
-  {
-    name: "Deloris Pettis",
-    job: "Vice Chairwoman, Director of Internal Audit",
-    organization: "Boston College",
-  },
-  {
-    name: "Joshua Schanck",
-    job:
-      "Chair of the ULEM Strategic Planning Committee, District Manager - Boston MA",
-    organization: "Starbucks",
-  },
-  {
-    name: "Malik A. Smith",
-    job: "Treasurer of the ULEM Board, First Vice President",
-    organization: "Private Wealth Advisor, Commons Capital Advisors",
-  },
-  {
-    name: "Mark Walker",
-    job: "Vice Chairman of the ULEM Board, Sr. VP, Credit Product Manager",
-    organization: "Citizens Bank",
-  },
+    {
+        name: "Joseph D. Feaster Jr.",
+        job: "Chairman of the ULEM Board, Of Counsel",
+        organization: "McKenzie & Associates, P.C."
+    },
+    {
+        name: "Linda Champion",
+        job: "Secretary of the ULEM Board, Attorney",
+        organization: ""
+    },
+    {
+        name: "Peter Dulac",
+        job: "Chair of the ULEM Program Committee, Vice President/General Manager",
+        organization: "Enterprise Holdings-Boston Group"
+    },
+    {
+        name: "Anthony Gaymes",
+        job: "Treasurer of the Board, Vice President of Commercial Lending",
+        organization: "TD Bank"
+    },
+    {
+        name: "Nikhil Goray",
+        job: "Chair of the ULEM Development Committee, Principal (Partner)",
+        organization: "Deloitte Consulting"
+    },
+    {
+        name: "Jeffrey L. Hirsch",
+        job: "Chair of the Personnel and Issue & Advocacy Committees, Attorney",
+        organization: "Hirsch Roberts Weinstein LLP"
+    },
+    {
+        name: "Vincent Loporchio",
+        job: "Chair of the ULEM Nominating Committee, Retired",
+        organization: "Fidelity Investments"
+    },
+    {
+        name: "Deloris Pettis",
+        job: "Vice Chairwoman, Executive Director in the Office of Internal Audit",
+        organization: "Boston College"
+    },
+    {
+        name: "Prasad Thottempudi",
+        job: "Chair of the ULEM Development Committee, Managing Director",
+        organization: "Deloitte Consulting"
+    },
+    {
+        name: "Joshua Schanck",
+        job: "Chair of the ULEM Strategic Planning Committee, District Manager - Boston MA",
+        organization: "Starbucks"
+    },
+    {
+        name: "Malik A. Smith",
+        job: "First Vice President,",
+        organization: "Private Wealth Advisor, Commons Capital Advisors"
+    },
+    {
+        name: "Mark Walker",
+        job: "Vice Chairman of the Board, Credit Delivery Group Manager/Senior Vice President",
+        organization: "Eastern Bank"
+    }
 ];
 
 export const membersOfTheBoard = [
-  {
-    name: "Catherine Burns",
-    job: "Partner",
-    organization: "Seyfarth Shaw LLP",
-  },
-  {
-    name: "Linda Champion",
-    job: "Attorney",
-    organization: "",
-  },
-  {
-    name: "John Dozier",
-    job: "Institute Community and Equity Officer (ICEO)",
-    organization: "MIT",
-  },
-  {
-    name: "Marian A. Green-Robinson",
-    job: "Vice President & Branch Manager, Stoughton Office",
-    organization: "Eastern Bank",
-  },
-  {
-    name: "Dr. Aisha E. Miller",
-    job: "Vice President of Permitting, Community and Corporate Engagement",
-    organization: "Related Beal",
-  },
-  {
-    name: "Lori Nelson",
-    job: "Chief Resilience Officer",
-    organization: "City of Boston",
-  },
-  {
-    name: "Christopher Scranton",
-    job: "Director of Nonprofit & Government Partnerships",
-    organization: "Jobcase",
-  },
-  {
-    name: "Billy Shore",
-    job: "Founder and Executive Chair",
-    organization: "Share our Strength",
-  },
-  {
-    name: "Prasad Thottempudi",
-    job: "Managing Director",
-    organization: "Deloitte Consulting",
-  },
-  {
-    name: "Jacquetta Van Zandt",
-    job: "Vice President of Engagement, The Partnership, Inc",
-    organization: "/ Interim YPN President",
-  },
-  {
-    name: "Erik White",
-    job: "Sr. Manager of Forensic & Integrity Services",
-    organization: "EY",
-  },
+    {
+        "name": "Catherine Burns",
+        "job": "Partner",
+        "organization": "Seyfarth Shaw LLP"
+    },
+    {
+        "name": "Evandro Carvalho",
+        "job": "Executive Director",
+        "organization": "Boston Human Rights Commission"
+    },
+    {
+        "name": "John Dozier",
+        "job": "Institute Community and Equity Officer (ICEO)",
+        "organization": "MIT"
+    },
+    {
+        "name": "Nikolin Gace",
+        "job": "Vice President/Business Banking Market Manager",
+        "organization": "Citizens Bank "
+    },
+    {
+        "name": "Marian A. Green-Robinson",
+        "job": "Vice President & Branch Manager, Stoughton Office",
+        "organization": "Eastern Bank"
+    },
+    {
+        "name": "Dr. Aisha E. Miller",
+        "job": "Vice President of Permitting, Community and Corporate Engagement",
+        "organization": "Related Beal"
+    },
+    {
+        "name": "Lori Nelson",
+        "job": "Chief Resilience Officer,",
+        "organization": "City of Boston"
+    },
+    {
+        "name": "Christopher Scranton",
+        "job": "Director of Nonprofit & Government Partnerships,",
+        "organization": "Jobcase"
+    },
+    {
+        "name": "Billy Shore",
+        "job": "Founder and Executive Chair",
+        "organization": "Share our Strength"
+    },
+    {
+        "name": "Prasad Thottempudi",
+        "job": "Managing Director",
+        "organization": "Deloitte Consulting"
+    },
+    {
+        "name": "Jacquetta Van Zandt",
+        "job": "Interim Pesident of YPN, Vice President of Engagement",
+        "organization": "The Partnership, Inc"
+    },
+    {
+        "name": "Erik White",
+        "job": "Sr. Manager of Forensic & Integrity Services",
+        "organization": "EY"
+    }
 ];
 
 export const boardChairsEmeritus = [
-  {
-    name: "Samuel J. Gerson",
-    job: "Chairman and Chief Executive",
-    organization: "Filene's Basement (Deceased)",
-  },
-  {
-    name: "Hon. Joyce London Alexander",
-    job: "Chief U.S. Magistrate Judge",
-    organization: "United States District Courthouse",
-  },
-  {
-    name: "Andrea Kershaw",
-    job: "Regional Vice President",
-    organization: "Enterprise Rent-A Car",
-  },
-  {
-    name: "Quincy L. Miller",
-    job: "President",
-    organization: "Eastern Bank",
-  },
-  {
-    name: "Jeffrey Musman",
-    job: "Managing Partner",
-    organization: "Seyfarth Shaw",
-  },
-  {
-    name: "George A. Russell Jr.",
-    job: "Executive Vice President",
-    organization: "State Street Corporation",
-  },
-  {
-    name: "Richard Taylor",
-    job: "Proprietor",
-    organization: "Taylor Smith Properties",
-  },
+    {
+        "name": "Samuel J. Gerson",
+        "job": "Chairman and Chief Executive",
+        "organization": "Filene's Basement (Deceased)"
+    },
+    {
+        "name": "Hon. Joyce London Alexander",
+        "job": "Chief U.S. Magistrate Judge",
+        "organization": "United States District Courthouse"
+    },
+    {
+        "name": "Andrea Kershaw",
+        "job": "Regional Vice President",
+        "organization": "Enterprise Rent-A Car"
+    },
+    {
+        "name": "Quincy L. Miller",
+        "job": "President",
+        "organization": "Eastern Bank"
+    },
+    {
+        "name": "Jeffrey Musman",
+        "job": "Managing Partner",
+        "organization": "Seyfarth Shaw"
+    },
+    {
+        "name": "George A. Russell Jr.",
+        "job": "Executive Vice President, Retired",
+        "organization": "State Street Corporation"
+    },
+    {
+        "name": "Richard Taylor",
+        "job": "Proprietor",
+        "organization": "Taylor Smith Properties"
+    }
 ];
 
 export const boardEmeritus = [
-  {
-    name: "Greg Almeida",
-    job: "President",
-    organization: "Global View Communication, Inc.",
-  },
-  {
-    name: "Gary Carroway",
-    job: "Director of Business Development",
-    organization: "UPS (Retired)",
-  },
-  {
-    name: "Petrina Cherry",
-    job: "Vice President of Community Engagement and External Affairs",
-    organization: "Boston Medical Center Health System",
-  },
-  {
-    name: "Brenda Cole",
-    job: "VP of Marketplace Inclusion Development",
-    organization: "Harvard Pilgrim Health Care",
-  },
-  {
-    name: "Bill Fenton",
-    job: "Senior Vice President",
-    organization: "Bank of America",
-  },
-  {
-    name: "Jacqueline Gadsden",
-    job: "VP of Diversity & Inclusion & Customer Experience",
-    organization: "Comcast - Greater Boston Area",
-  },
-  {
-    name: "Johnny Green Jr.",
-    job: "VP of News Services",
-    organization: "CBS News",
-  },
-  {
-    name: "Dorothea Jones",
-    job: "ULEM Guild President, NCULG Guild Secretary",
-    organization: "National Urban League",
-  },
+    {
+        "name": "Greg Almeida",
+        "job": "President",
+        "organization": "Global View Communication, Inc."
+    },
+    {
+        "name": "Gary Carroway",
+        "job": "Director of Business Development",
+        "organization": "UPS (Retired)"
+    },
+    {
+        "name": "Petrina Cherry",
+        "job": "Vice President of Community Engagement and External Affairs",
+        "organization": "Boston Medical Center Health System"
+    },
+    {
+        "name": "Brenda Cole",
+        "job": "VP of Marketplace Inclusion Development",
+        "organization": "Harvard Pilgrim Health Care"
+    },
+    {
+        "name": "Bill Fenton",
+        "job": "Senior Vice President",
+        "organization": "Bank of America"
+    },
+    {
+        "name": "Jacqueline Gadsden",
+        "job": "VP of Diversity & Inclusion & Customer Experience",
+        "organization": "Comcast - Greater Boston Area"
+    },
+    {
+        "name": "Johnny Green Jr.",
+        "job": "VP of News Services",
+        "organization": "CBS News"
+    },
+    {
+        "name": "Dorothea Jones",
+        "job": "ULEM Guild President, NCULG Guild Secretary",
+        "organization": "National Urban League"
+    }
 ];
 
 export const advisoryCouncil = [
-  {
-    name: "Scott Baker",
-    job: "President",
-    organization: "SkyCom Courier",
-  },
-  {
-    name: "Donnie Bedney III",
-    job: "Operating Partner",
-    organization: "ScaleCo",
-  },
-  {
-    name: "Larry Dunham",
-    job: "Director of Diversity and Multicultural Affairs",
-    organization: "Boston University/Dental School",
-  },
-  {
-    name: "David Gibbons",
-    job: "Executive Director",
-    organization: "Massachusetts Convention Center Authority (MCCA)",
-  },
-  {
-    name: "Dorothea Jones",
-    job: "President of The GUILD",
-    organization: "The Urban League of Eastern MA",
-  },
-  {
-    name: "Pam Kenn",
-    job: "VP of Community, Alumni and Player Relations",
-    organization: "Boston Red Sox",
-  },
-  {
-    name: "Lisa Timberlake",
-    job: "Media Relations",
-    organization: "City of Boston",
-  },
+    {
+        "name": "Scott Baker",
+        "job": "President",
+        "organization": "SkyCom Courier"
+    },
+    {
+        "name": "Donnie Bedney III",
+        "job": "Operating Partner",
+        "organization": "ScaleCo"
+    },
+    {
+        "name": "Larry Dunham",
+        "job": "Director of Diversity and Multicultural Affairs",
+        "organization": "Boston University/Dental School"
+    },
+    {
+        "name": "David Gibbons",
+        "job": "Executive Director",
+        "organization": "Massachusetts Convention Center Authority (MCCA)"
+    },
+    {
+        "name": "Dorothea Jones",
+        "job": "President of The GUILD",
+        "organization": "The Urban League of Eastern MA"
+    },
+    {
+        "name": "Lisa Timberlake",
+        "job": "Media Relations",
+        "organization": "City of Boston"
+    }, {
+        "name": "Scott Baker",
+        "job": "President",
+        "organization": "SkyCom Courier"
+    },
+    {
+        "name": "Donnie Bedney III",
+        "job": "Operating Partner",
+        "organization": "ScaleCo"
+    },
+    {
+        "name": "Larry Dunham",
+        "job": "Director of Diversity and Multicultural Affairs",
+        "organization": "Boston University/Dental School"
+    },
+    {
+        "name": "David Gibbons",
+        "job": "Executive Director",
+        "organization": "Massachusetts Convention Center Authority (MCCA)"
+    },
+    {
+        "name": "Dorothea Jones",
+        "job": "President of The GUILD",
+        "organization": "The Urban League of Eastern MA"
+    },
+    {
+        "name": "Lisa Timberlake",
+        "job": "Media Relations",
+        "organization": "City of Boston"
+    }
 ];
