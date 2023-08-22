563cf789792c830c58eebee6946836d945984488
Dele Omotosho <dele@authoritywit.com>
2023-08-22 13:46:26 -0400

    adds MBE info

--- src/components/program/Programs.js
+++ src/components/program/Programs.js
@@ -1,116 +1,196 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 export default class Programs extends Component {
-  render() {
-    return (
-      <section id="program">
-        <h1 className="font-red mt-4 mb-4">
-          Workforce Development Training Programs
-        </h1>
-        <h4>MSIMBO: ULEM Coding Bootcamp</h4>
-        <p>
-          Msimbo means “code” in Swahili. A twenty (20) week program for
-          displaced, unemployed and underemployed workers interested becoming
-          computer programmers. This programs seeks to address the IT
-          industries’ issue of diversity and skilled workers. ULEM looks to
-          enroll 18 students and provide a stipend for $100/ week.
-        </p>
-        <p>
-          MSIMBO bootcamp trains talented, low-income individuals to become
-          full-stack web developers and launch a new career. Students are
-          trained in web development, effective business communication, and
-          leadership. We will conduct a 20 weeks of 120 hours coding technology
-          training program and the professional development for ULEM’s MSIMBO
-          program.
-        </p>
-        <p>The targeted population must: </p>
-        <ul className="pl-2">
-          <li className="inline mr-2">
-            <i className="fas fa-check font-red" />
-            Be at least 18 years of age
-          </li>
-          <li className="inline mr-2">
-            <i className="fas fa-check font-red" />
-            Have a high school diploma or GED
-          </li>
-          <li className="inline">
-            <i className="fas fa-check font-red" />
-            Be committed to completing the program
-          </li>
-        </ul>
-        <p>
-          MSIMBO: ULEM Coding Bootcamp will offer during the 20 weeks,
-          participants are trained in:
-        </p>
-        <ul className="list-no-style">
-          <li>
-            <i className="fas fa-book mr-2 font-red" />
-            Web Development - Learning to make Web Applications (HTML, CSS,
-            JavaScript, MEAN Stack)
-          </li>
-          <li>
-            <i className="fas fa-book mr-2 font-red" />
-            Financial Literacy – Our Dollars & Sense Training
-          </li>
-          <li>
-            <i className="fas fa-book mr-2 font-red" />
-            National Career Readiness testing and certification
-          </li>
-          <li>
-            <i className="fas fa-book mr-2 font-red" />
-            Resume, Cover Letter & Linkedin Profile Building
-          </li>
-        </ul>
-        <p>
-          Those who successfully complete the program will be eligible for
-          recommendation for employment, internships or apprenticeships with
-          various Tech companies in the area. Throughout the program case
-          management and job placement services are available. Those who are not
-          placed receive job placement assistance from ULEM. Stipend based
-          program.<strong>SECTION 30 APPROVED.</strong>
-        </p>
-        <p>
-          For more information visit:{" "}
-          <a
-            href="https://msimbo.org"
-          >
-            https://www.msimbo.org
-          </a>
-        </p>
-        <hr />
-        <h4>DiGiUL Program</h4>
-        <p>
-          The DiGiUL program is at full time Monday through Friday 10am – 4pm
-          classroom setting program. That teaches you all the fundamentals of
-          graphic design, branding, marketing – both from the print and
-          interactive perspective. At the end of the 16 week program you will
-          have the knowledge to land a job in the field working as a design
-          professional, you will have a professional level portfolio – both
-          print and online that will have been vetted by working professionals
-          in the field and you will have had some working experience in said
-          field. Those who successfully complete program will be eligible for
-          recommendation for employment, internships or apprenticeships with
-          various ad agencies and marketing companies in the area. Throughout
-          the program case management and job placement services are available.
-          Those who are not placed receive job placement assistance from ULEM.
-          Stipend based program. <strong>SECTION 30 APPROVED.</strong>
-        </p>
-        <hr />
-        <h3>Computer Literacy and Internet Knowledge Program</h3>
-        <p>
-          Powered by the Timothy Smith Network, Computer Literacy and Internet
-          Knowledge program (CLIK) is a computer literacy training program for
-          individuals who lack basic computer skills critical for job search and
-          employment success. During this 5-week course, CLIK teaches Computing
-          Fundamentals, Key Applications, and Living Online. The Computing
-          Fundamentals covers a foundational understanding of computer hardware,
-          software, operating systems, peripherals, and troubleshooting to help
-          you get the most value and impact from computer technology. Students
-          that complete the CLIK program are eligible to receive a Google
-          Chromebook.
-        </p>
-        <hr />
-        {/* <h3>Mature Worker Skills Program (MWSP)</h3>
+    render() {
+        return (<section id="program">
+            <h1 className="font-red mt-4 mb-4">
+                Workforce Development Training Programs
+            </h1>
+            <h4>MSIMBO: ULEM Coding Bootcamp</h4>
+            <p>
+                Msimbo means “code” in Swahili. A twenty (20) week program for
+                displaced, unemployed and underemployed workers interested becoming
+                computer programmers. This programs seeks to address the IT
+                industries’ issue of diversity and skilled workers. ULEM looks to
+                enroll 18 students and provide a stipend for $100/ week.
+            </p>
+            <p>
+                MSIMBO bootcamp trains talented, low-income individuals to become
+                full-stack web developers and launch a new career. Students are
+                trained in web development, effective business communication, and
+                leadership. We will conduct a 20 weeks of 120 hours coding technology
+                training program and the professional development for ULEM’s MSIMBO
+                program.
+            </p>
+            <p>The targeted population must: </p>
+            <ul className="pl-2">
+                <li className="inline mr-2">
+                    <i className="fas fa-check font-red"/>
+                    Be at least 18 years of age
+                </li>
+                <li className="inline mr-2">
+                    <i className="fas fa-check font-red"/>
+                    Have a high school diploma or GED
+                </li>
+                <li className="inline">
+                    <i className="fas fa-check font-red"/>
+                    Be committed to completing the program
+                </li>
+            </ul>
+            <p>
+                MSIMBO: ULEM Coding Bootcamp will offer during the 20 weeks,
+                participants are trained in:
+            </p>
+            <ul className="list-no-style">
+                <li>
+                    <i className="fas fa-book mr-2 font-red"/>
+                    Web Development - Learning to make Web Applications (HTML, CSS,
+                    JavaScript, MEAN Stack)
+                </li>
+                <li>
+                    <i className="fas fa-book mr-2 font-red"/>
+                    Financial Literacy – Our Dollars & Sense Training
+                </li>
+                <li>
+                    <i className="fas fa-book mr-2 font-red"/>
+                    National Career Readiness testing and certification
+                </li>
+                <li>
+                    <i className="fas fa-book mr-2 font-red"/>
+                    Resume, Cover Letter & Linkedin Profile Building
+                </li>
+            </ul>
+            <p>
+                Those who successfully complete the program will be eligible for
+                recommendation for employment, internships or apprenticeships with
+                various Tech companies in the area. Throughout the program case
+                management and job placement services are available. Those who are not
+                placed receive job placement assistance from ULEM. Stipend based
+                program.<strong>SECTION 30 APPROVED.</strong>
+            </p>
+            <p>
+                For more information visit:{" "}
+                <a
+                    href="https://msimbo.org"
+                >
+                    https://www.msimbo.org
+                </a>
+            </p>
+            <hr/>
+            <h4>DiGiUL Program</h4>
+            <p>
+                The DiGiUL program is at full time Monday through Friday 10am – 4pm
+                classroom setting program. That teaches you all the fundamentals of
+                graphic design, branding, marketing – both from the print and
+                interactive perspective. At the end of the 16 week program you will
+                have the knowledge to land a job in the field working as a design
+                professional, you will have a professional level portfolio – both
+                print and online that will have been vetted by working professionals
+                in the field and you will have had some working experience in said
+                field. Those who successfully complete program will be eligible for
+                recommendation for employment, internships or apprenticeships with
+                various ad agencies and marketing companies in the area. Throughout
+                the program case management and job placement services are available.
+                Those who are not placed receive job placement assistance from ULEM.
+                Stipend based program. <strong>SECTION 30 APPROVED.</strong>
+            </p>
+            <hr/>
+            <h3>Next Stage MBE Academy</h3>
+            <p></p>
+            <p>
+                <strong><em>Feeling isolated as a small business owner or has revenue or profit plateaued or even
+                    started to decline?</em></strong></p>
+            <p></p>
+            <p><em><strong>NextStage MBE Academy powered by Interise’s award winning curriculum could be perfect for you
+                in helping to begin to work on creating a path to get to your next stage of growth and to scaling your
+                business!</strong></em></p>
+            <blockquote><p><em>“When I started this program I was struggling. It didn&#39;t matter if it was a good or
+                bad economy. Our sales were flat. We couldn&#39;t increase sales and were always in the hole. I entered
+                the program, and the instructor made a lot of promises that I didn&#39;t believe were going to be true,
+                but assured me they would. I would go into class every session thinking I had all the answers, and I
+                would leave, wondering how I was still in business. Every class I went to I always left thinking and
+                trying to figure out what I was doing wrong, and I would always get my answers from my other classmates.
+                After graduating I finally understood how to run a business instead of how to do my job. Sales
+                increased, doubled, doubled, and doubled to a point where we got 7 or 8 full-time employees. Most of the
+                employees have been with me since I took the class 10 years ago. Most of them were hourly employees and
+                now they&#39;re all homeowners.”</em></p>
+            </blockquote>
+            <p>The Urban League of Eastern Massachusetts has partnered with Interise to develop a state-wide initiative
+                to support small businesses to grow to scale. Today we personally want you to be a participant in our
+                NextStage MBE MBE Academy powered by Interise! Three new cohorts are beginning in the areas of Boston,
+                Brockton and South Coast. <strong>If you do business in these areas, we invite you to apply
+                    today!</strong> <strong>Don’t Wait- Apply by Clicking Here:</strong> <a
+                    href="https://interise.org/nextstage-mbe-form-submitted/" target="_blank"
+                    className="url">https://interise.org/nextstage-mbe-form-submitted/</a></p>
+            <ul>
+                <li><p><strong>SouthCoast Cohort Application Deadline Dates</strong></p>
+                    <ul>
+                        <li> - Thursday September 7th at 12:00 noon est.</li>
+
+                    </ul>
+                </li>
+                <br/>
+                <li><p><strong>Boston Cohort Application Deadline Dates</strong></p>
+                    <ul>
+                        <li>- Monday October 9th at 12:00 noon est.</li>
+
+                    </ul>
+                </li>
+                <br/>
+                <li><p><strong>Brockton Cohort Application Deadline</strong></p>
+                    <ul>
+                        <li>- Tuesday October 10th at 12:00 noon est.</li>
+                    </ul>
+                </li>
+            </ul>
+            <p>Since 2004 the Interise award winning curriculum has over 9,000 graduates, over 500 in the Commonwealth
+                of Massachusetts that employ over 150,000 nationally with <strong>an average revenue increase of
+                    $500k</strong> per business on average, and 97% of alumni businesses would recommend the program to
+                a fellow business owner. This winning Interise curriculum is proven to scale businesses and increase
+                revenue while building the business owners&#39; capacity and confidence. In addition, it’s designed to
+                provide a practical executive level small business education and ensure businesses have an opportunity
+                to reach their full potential. </p>
+            <p><strong>By joining a cohort in the Boston, South Shore, or South Coast areas</strong> you will join a
+                supportive community of local business owners and engage in interactive class sessions, peer mentoring
+                groups, and subject matter experts led by local leaders in finance, marketing, and procurement experts.
+            </p>
+            <p><strong>By program end, participants will have developed a customized 3-year strategic growth plan and
+                the confidence and skills to bring these plans to life</strong>. Take advantage of this opportunity for
+                you to invest in the success of your business. </p>
+            <p>We are building a solid cohort and I want you to participate! <strong>Apply Here:</strong> <a
+                href="https://interise.org/nextstage-mbe-form-submitted/" target="_blank"
+                className="url">https://interise.org/nextstage-mbe-form-submitted/</a></p>
+            <p><strong>BASIC ELIGIBILITY REQUIREMENTS</strong></p>
+            <ul>
+                <li>- Businesses that are located in the Boston, Brockton, New Bedford, Fall River, and surrounding
+                    areas.
+                </li>
+                <li>- Minority or female business owner maintaining at least 51% business ownership</li>
+                <li>- Annual revenue between $125,000 and $10,000,000</li>
+                <li>- A minimum of one employee besides the owner(s).</li>
+                <li>- Been in business for at least 2 years.</li>
+                <li>- Desiring to work “on” and not just “in” your business</li>
+                <li>- Personalized 3-year Strategic Growth Plan</li>
+
+            </ul>
+            <p></p>
+
+            <hr/>
+            <h3>Computer Literacy and Internet Knowledge Program</h3>
+            <p>
+                Powered by the Timothy Smith Network, Computer Literacy and Internet
+                Knowledge program (CLIK) is a computer literacy training program for
+                individuals who lack basic computer skills critical for job search and
+                employment success. During this 5-week course, CLIK teaches Computing
+                Fundamentals, Key Applications, and Living Online. The Computing
+                Fundamentals covers a foundational understanding of computer hardware,
+                software, operating systems, peripherals, and troubleshooting to help
+                you get the most value and impact from computer technology. Students
+                that complete the CLIK program are eligible to receive a Google
+                Chromebook.
+            </p>
+            <hr/>
+            {/* <h3>Mature Worker Skills Program (MWSP)</h3>
         <p>
           In partnership with the Brigham & Women Hospital, the MWSP program is
           for individuals 45 or older who are unemployed and seeking a way to
@@ -121,18 +201,17 @@ export default class Programs extends Component {
           free life. Enroll Today!
         </p>
         <hr /> */}
-        <h3>Customer Service Sales Training (CSST)</h3>
-        <p>
-          This training prepares participants for work through classroom
-          learning and meaningful service activities. Student prepare for the
-          Rise Up ®, Customer Service and Sales Certification through the
-          National Retailer Federation. It is designed for people who are
-          currently unemployed, have limited work experience, and want to be job
-          ready. The CSST program gives graduates the work experience they need
-          to build their resumes and promote their skills effectively. This is a
-          stipend eligible program. Enroll Today!
-        </p>
-      </section>
-    );
-  }
+            <h3>Customer Service Sales Training (CSST)</h3>
+            <p>
+                This training prepares participants for work through classroom
+                learning and meaningful service activities. Student prepare for the
+                Rise Up ®, Customer Service and Sales Certification through the
+                National Retailer Federation. It is designed for people who are
+                currently unemployed, have limited work experience, and want to be job
+                ready. The CSST program gives graduates the work experience they need
+                to build their resumes and promote their skills effectively. This is a
+                stipend eligible program. Enroll Today!
+            </p>
+        </section>);
+    }
 }
